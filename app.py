017c16abec36b8e81f42e396089c275379b06e75
muratbakirr <153373667+muratbakirr@users.noreply.github.com>
2024-08-22 14:43:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
-from sklearn.preprocessing import StandardScaler
 
 def load_model(model_path, scaler_path):
     model = joblib.load(model_path)
